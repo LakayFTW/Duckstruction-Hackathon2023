12783bac4627253965c993140f542e595c458a8f
Luca de Graaf <Luca.de-graaf@ewetel.net>
2023-02-18 08:41:32 +0100

    changes und so

--- test.py
+++ test.py
@@ -4,15 +4,13 @@ from const import SECRET, URL
 from handler import handlers
 
 sio = socketio.AsyncClient()
-field = [['']*10]*10
+
 @sio.event
 async def connect():
-    print('Verbunden!')
     await sio.emit('authenticate', SECRET)    
 
 @sio.event
 async def data(data):
-    print('Daten vom Typ:' + str(data['type']))
     t = data['type']
     if t in handlers:
         return handlers[t](data)
